54ba3760c583e18d09af465d433e276f7cd1b091
raymond98tan <raymond98tan@gmail.com>
2020-04-23 16:48:29 -0400

    added body parser

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-
+const bodyparser = require("body-parser");
 const app = express();
 
 const port = 80;
@@ -7,7 +7,7 @@ const port = 80;
 require("./routes")(app)
 app.use(express.json())
 app.use(express.static("./static"))
-
+app.use(bodyparser.urlencoded({extended:false}))
 app.listen(port,()=>{
     console.log("server live on " + port)
 })
