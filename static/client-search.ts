222cf3ea46caf00fd21484b27b0078438d58771f
Ankita Kumar <ankitasemail@gmail.com>
2020-04-24 18:39:34 -0400

    initialized HTML inputs for search, added ids for dropdown in index.html

---
+++ static/client-search.ts
@@ -0,0 +1,29 @@
+//so mainly
+//make it activate when they click the search button
+//read things from the html
+//pass them to the backend
+//get results from the backend
+//post them to the html
+//using a lot of html.innerelement and outerelement baiscally
+//linking to index.html
+
+
+
+export function search() {
+    (async () => {
+        let buildingElement = document.getElementById("building") as HTMLInputElement;
+        let genderElement = document.getElementById("gender") as HTMLInputElement;
+        let accessibleElement = document.getElementById("accessible") as HTMLInputElement;
+        let lactationElement = document.getElementById("lactation") as HTMLInputElement;
+        let changingElement = document.getElementById("changing") as HTMLInputElement;
+        let stallElement = document.getElementById("stall") as HTMLInputElement; 
+        let towelsElement = document.getElementById("towels") as HTMLInputElement;
+        let coversElement = document.getElementById("covers") as HTMLInputElement;
+        let sanitaryElement = document.getElementById("sanitary") as HTMLInputElement;
+        let lockElement = document.getElementById("lock") as HTMLInputElement;
+
+    const data = {}
+    
+    
+    })();
+}
