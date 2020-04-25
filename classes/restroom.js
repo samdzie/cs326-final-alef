f20c40da3e339cebba36910b4ad5b7acf73b2572
Sam Dziewietin <ssdz10@gmail.com>
2020-04-25 01:53:02 -0400

    Merge to master

---
+++ classes/restroom.js
@@ -0,0 +1,27 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const comments_1 = require("./comments");
+class Features {
+    constructor() {
+        this.gender = "Neutral";
+        this.accessible = false;
+        this.lactation = false;
+        this.changing = false;
+        this.stall = false;
+        this.towels = false;
+        this.covers = false;
+        this.sanitary = false;
+        this.lock = false;
+    }
+}
+exports.Features = Features;
+class Restroom {
+    constructor(id) {
+        this.id = id;
+        this.name = "";
+        this.description = "";
+        this.features = new Features();
+        this.comments = new comments_1.CommentSection();
+    }
+}
+exports.Restroom = Restroom;
---
+++ classes/user.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class User {
+    constructor(username) {
+        this.username = username;
+    }
+}
+exports.User = User;
