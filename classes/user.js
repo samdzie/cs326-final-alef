f7bf3db6a2a7879a3273e6005cd6ae92f5417dc1
samdzie <ssdz10@gmail.com>
2020-05-01 16:32:31 -0400

    Remove compiled JS files

--- classes/user.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var User = /** @class */ (function () {
-    function User(username) {
-        this.username = username;
-    }
-    return User;
-}());
-exports.User = User;
