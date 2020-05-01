0c7dc72239c8a2f4e450bc0ff288e3b3ce65cc0d
samdzie <ssdz10@gmail.com>
2020-05-01 19:58:09 -0400

    Create build script

---
+++ build.sh
@@ -0,0 +1,4 @@
+tsc
+browserify static/client-restroom.js -o static/client-restroom-main.js --standalone client
+browserify static/client-search.js -o static/client-search-main.js --standalone client
+browserify static/client-user.js -o static/client-user-main.js --standalone client
