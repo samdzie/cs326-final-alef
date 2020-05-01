b2ab616d5f8988e1a2e9ebbfe82bebaef1f93f91
samdzie <ssdz10@gmail.com>
2020-05-01 16:29:45 -0400

    Remove and gitignore compiled JS files

--- server-main.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-exports.__esModule = true;
-var myserver_post_1 = require("./myserver-post");
-// const theDatabase = new Database('samdzie'); // CHANGE THIS
-// const theServer = new MyServer(theDatabase);
-var theServer = new myserver_post_1.MyServer();
-theServer.listen(8080);
