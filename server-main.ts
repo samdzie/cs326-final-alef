df371db9175216a42d9b51c8aec7caff49261437
samdzie <ssdz10@gmail.com>
2020-05-03 01:57:01 -0400

    Change main collection name to 'restroom'

--- server-main.ts
+++ server-main.ts
@@ -5,7 +5,7 @@ const port = process.env.PORT || 8080;
 import { Database } from './mongo-database';
 import { MyServer } from './myserver-post';
 
-const theDatabase = new Database('main'); // CHANGE THIS
-const theServer = new MyServer(theDatabase);
+const database = new Database("restroom");
+const server = new MyServer(database);
 
-theServer.listen(port);
+server.listen(port);
